4ffe41a26113f446551403bd490396044c3948e0
RR <rrn091301@gmail.com>
2025-03-17 14:29:25 +0800

    fix: Add functionality to mobile navbar menu items

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const mobileMenu = document.querySelector(".mobile-menu");
 const navLinksList = document.querySelector("#nav-links-list");
+const navLinks = document.querySelectorAll("#nav-links-list > li");
 
 mobileMenu.addEventListener("click", mobileMenuOpen);
+
+const about = new Event("click");
 
 function mobileMenuOpen(e) {
     mobileMenu.classList.toggle("close");
     navLinksList.classList.toggle("menu-closed");
 }
+
+navLinks.forEach((item)=> {
+    item.addEventListener("click", () => {
+        const anchor = item.querySelector('a');
+        if (anchor) {
+            window.location.href = anchor.href;
+        }
+    });
+});
